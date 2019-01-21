efcc3fdd7923f7400418fe97eeacbf4e72ac7e03
EnzoDominguesMe <edomingues@futurebrand.com>
2019-01-21 10:25:23 -0200

    first commit

---
+++ src/components/Map/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import MapView from 'react-native-maps'
+
+
+export default class Map extends Component {
+  state = {
+    region: null,
+  }
+
+  async componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        this.setState({
+          region: {
+            latitude,
+            longitude,
+            latitudeDelta: 0.0143,
+            longitudeDelta: 0.0134
+          }
+        });
+      }, //success
+      () => {}, //error
+      {
+        timeout: 5000,
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+      }
+    )
+  }
+
+  render() {
+    const { region } = this.state
+   return (
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          region={region}
+          showsUserLocation
+          loadingEnabled
+        />
+      </View>
+    )
+  }
+}
+
