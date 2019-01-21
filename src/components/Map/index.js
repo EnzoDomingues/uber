c4e69800536b1fb3b4861915340e4f0466f6d077
EnzoDominguesMe <edomingues@futurebrand.com>
2019-01-21 15:13:54 -0200

    line direction ready

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import MapView from 'react-native-maps'
+import React, { Component } from "react";
+import { View } from "react-native";
+import MapView from "react-native-maps";
 
+import Search from "../Search";
+import Directions from "../Directions";
 
 export default class Map extends Component {
   state = {
     region: null,
-  }
+    destination: null
+  };
 
   async componentDidMount() {
     navigator.geolocation.getCurrentPosition(
@@ -24,23 +27,58 @@ export default class Map extends Component {
       {
         timeout: 5000,
         enableHighAccuracy: true,
-        maximumAge: 1000,
+        maximumAge: 1000
       }
-    )
+    );
   }
 
+  handleLocationSelected = (data, { geometry }) => {
+    console.log("entrou");
+
+    const {
+      location: { lat: latitude, lng: longitude }
+    } = geometry;
+
+    this.setState({
+      destination: {
+        latitude,
+        longitude,
+        title: data.structured_formatting.main_text
+      }
+    });
+  };
+
   render() {
-    const { region } = this.state
-   return (
+    const { region, destination } = this.state;
+    return (
       <View style={{ flex: 1 }}>
         <MapView
           style={{ flex: 1 }}
           region={region}
           showsUserLocation
           loadingEnabled
-        />
+          ref={(el) => (this.mapView = el)}
+        >
+          {destination && (
+            <Directions
+              origin={region}
+              destination={destination}
+              onReady={(result) => {
+                this.mapView.fitToCoordinates(result.coordinates, {
+                  edgePadding: {
+                    right: 50,
+                    left: 50,
+                    top: 50,
+                    bottom: 50
+                  }
+                });
+              }}
+            />
+          )}
+        </MapView>
+
+        <Search onLocationSelected={this.handleLocationSelected} />
       </View>
-    )
+    );
   }
 }
-
